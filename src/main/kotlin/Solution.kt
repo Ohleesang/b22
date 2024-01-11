3e1ccd4e89c8e5c1bce9510fd7ae55b316a0aec1
Ohleesang <jammmmed@gmail.com>
2024-01-11 09:40:25 +0900

    reversedArray() 사용

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,12 @@
 class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        answer = num_list.reversedArray()
+        return answer
+    }
+}
+fun main(){
+    Solution().solution(intArrayOf(1,2,3,4,5))
+    Solution().solution(intArrayOf(1,1,1,1,1,2))
+    Solution().solution(intArrayOf(1,0,1,1,1,3,5))
 }
